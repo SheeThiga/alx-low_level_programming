16d374f9926c11cbc0d989e15d7888651fc7baf7
SheeThiga <thigamercy399@gmail.com>
2023-07-21 13:39:28 -0700

    diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,21 +5,23 @@
  * @n: number of times the '\' should be printed
  * Return: void
  */
-
 void print_diagonal(int n)
 {
 	if (n <= 0)
-		_putchar('\n');
-	else
 	{
-		int i, j;
+		_putchar('\n');
+		return;
+	}
 
-		for (i = 0 ; i < n ; i++)
+	for (int i = 0; i < n; i++)
+	{
+
+		for (int j = 0; j < i; j++)
 		{
-			for (j = 0 ; j <= i ; j++)
-				_putchar(' ');
-			_putchar('\\');
-			_putchar('\n');
+
+			_putchar(' ');
 		}
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
